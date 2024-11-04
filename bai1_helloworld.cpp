8f8f74cc32d2c575308450a6b3e56de44f7059c8
lehoangtruonghuy2009@gmail.com <MAY 06@LAB2-17>
2024-11-04 11:24:34 +0700

    Add project files.

---
+++ bai1_helloworld.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	cout << "hello world!\n";
+	cout << "TOI TEN LA_" << "\tle hoang truong huy" << endl;
+	cout << "toi dang choi \tc++" << endl;
+		return 0;
+}
